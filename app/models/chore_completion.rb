c0696e1f23615183adf3ecfc3d1f807bbbfbf378
Mike Mayo <mmayo@kalkomey.com>
2016-02-10 21:26:37 -0600

    can't complete a chore twice in the same hour (sane default)

--- app/models/chore_completion.rb
+++ app/models/chore_completion.rb
@@ -1,4 +1,13 @@
 class ChoreCompletion < ActiveRecord::Base
   belongs_to :chore
   belongs_to :user
+
+  validate :unique_within_timeframe, on: :create
+
+  def unique_within_timeframe
+    matching = ChoreCompletion.where(chore_id: self.chore_id, user_id: self.user_id).where("created_at > ?", 1.hour.ago)
+    if matching.any?
+      errors.add(:chore_id, "has already been completed")
+    end
+  end
 end
